ccfc1d0b0ec8a0e7caca2344a3851ceecde2d54d
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-28 23:01:06 +0200

    step6

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ app.get("/time", (req, res) => {
     res.send({ status: 200, message: time });
 });
 app.get('/hello/:username', (req, res) => {
-    res.json({status:200, message: `Hello, ${req.params.username}`})
+    res.json({ status: 200, message: `Hello, ${req.params.username}` })
 })
 
 app.get('/search', (req, res) => {
     console.log(req.query.s)
-    if(req.query.s == undefined || req.query.s == "") {
+    if (req.query.s == undefined || req.query.s == "") {
         console.log(req.query.s)
-        res.json({status:500, error:true, message:"you have to provide a search" })
-    } 
+        res.json({ status: 500, error: true, message: "you have to provide a search" })
+    }
     else {
-        res.json({status:200, message:"ok", data: req.query.s})
+        res.json({ status: 200, message: "ok", data: req.query.s })
     }
 });
 const movies = [
@@ -38,18 +38,29 @@ const movies = [
     { title: 'Brazil', year: 1985, rating: 8 },
     { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 ]
-app.get("/movies/create",(req,res)=>{
+app.get("/movies/create", (req, res) => {
     res.send(`create`)
 });
-app.get("/movies/read" ,(req,res)=>{
-    res.json({status:200,data:movies});
+app.get("/movies/read", (req, res) => {
+    res.json({ status: 200, data: movies });
 });
-app.get("/movies/update",(req,res)=>{
+app.get("/movies/update", (req, res) => {
     res.send(`update`);
 })
-app.get("/movies/delete",(req,res)=>{
+app.get("/movies/delete", (req, res) => {
     res.send(`delete`);
 })
+app.get('/movies/read/by-date', (req, res) => {
+    res.json({ status: 200, data: movies.sort((a, b) => a.year - b.year) });
+});
+app.get("/movies/read/by-rating", (req, res) => {
+    res.json({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) })
+})
+
+app.get("/movies/read/by-title", (req, res) => {
+    res.json({status: 200, data: movies.sort((a, b) => (a.title).localeCompare(b.title))
+    })
+});
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
 })
