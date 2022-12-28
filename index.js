02694cff4e70f63eb41639c44d7ce38794992e4d
Moemenhaydar <moemen.haydar98@gmail.com>
2022-12-28 21:26:48 +0200

    step3

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(core())
 app.get("/", core(), async (req, res) => {
     res.send("ok")
 })
+app.get("/test", (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+    const currentDate = new Date();
+    const time = currentDate.getHours() + ":" + currentDate.getMinutes();
+    res.send({ status: 200, message: time });
+});
+
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
